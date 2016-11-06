e3d6b0bbbb0ecdf3afef4f2c03e9107328c75c35
EstebanMV96 <moyaco41@gmail.com>
2016-11-06 11:09:41 -0500

    Update test_files.py

--- test_files.py
+++ test_files.py
@@ -18,6 +18,12 @@ def recently(client):
 	return client.get('/files/recently_created',follow_redirects=True)
 
 
+def crearArchivo(client):
+	arch='prueba.txt'
+	response = client.post('/files',data=json.dumps(dict(filename=arch,content='Hola mundo')),content_type='application/json')
+	return response
+
+
 def test_darArchivos(client):
 	result=darArchivos(client)
 	esta="comandos.py" in result.data
@@ -25,6 +31,7 @@ def test_darArchivos(client):
 	assert esta, "comandos.py deberia de aparecer en los archivos listados"
 	assert esta1, "files.py deberia de aparecer en los archivos listados"
 	assert result.status=="200 OK", "El codigo de respuesta indica un error"
+	
 	
 	
 	
@@ -37,3 +44,7 @@ def test_eliminarArchivos(client):
 def test_recently(client):
 	result=recently(client)
 	assert result.status=="200 OK", "El codigo de respuesta no fue el esperado"
+
+def test_crearArchivo(client):
+	result=crearArchivo(client)
+	assert result.status="201 CREATED", "Ocurrio un error al crear el archivo"
